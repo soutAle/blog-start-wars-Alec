e974f667c76281bb425c03a59dc72278188807fa
Alejandro Valencia <99873279+soutAle@users.noreply.github.com>
2025-01-09 20:50:44 +0000

    correct

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -1,9 +1,9 @@
-import React, { Component } from "react";
+import React from "react";
 
 export const Footer = () => (
 	<footer className="footer mt-5 py-3 text-center bg-dark">
 		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
+			Made with <i className="fa fa-heart text-danger" /> by{"Alec"}
 			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
 		</p>
 	</footer>
